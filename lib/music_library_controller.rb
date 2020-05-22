c2ee933ced29943b8ef6eeb4be7ffc88f220d2c9
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-22 18:06:40 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
   
   def list_songs
     Song.all.sort{|a, b| a.name <=> b.name}.each_with_index(1) do |a, i|
-      puts ""
+      puts "#{i}"
   end
   def list_artists
   end
