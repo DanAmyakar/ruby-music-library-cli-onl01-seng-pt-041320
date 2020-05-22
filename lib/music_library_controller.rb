3a8758475c66fd54fd9e91d9f1f52c82963e1daa
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-22 18:02:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -43,7 +43,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort{|a, b| a.name <=> b.name 
+    Song.all.sort{|a, b| a.name <=> b.name}.each
   end
   def list_artists
   end
