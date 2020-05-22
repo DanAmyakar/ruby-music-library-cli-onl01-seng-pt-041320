ec2296743c48ad798ede5fcd61fd042ef870938d
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-22 19:30:55 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -87,6 +87,8 @@ class MusicLibraryController
     if (1..song_list.length).include?(input-1)
       song_from_list = song_list[input-1]
       puts "Playing #{song_from_list.name} by #{song_from_list.artist.name}"
+    else
+      nil
     end
   end
   
